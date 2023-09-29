04398a72819c14b3ae4e1feab54a64d5cafd8cee
Bojken07 <bojken.zaimi@stud.sti.se>
2023-09-29 18:40:35 +0200

    Breakpoint

--- src/com/felhantering/problemlosning/demo/Main.java
+++ src/com/felhantering/problemlosning/demo/Main.java
@@ -1,4 +1,13 @@
 package com.felhantering.problemlosning.demo;
 
 public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 0; i-- ) {
+            System.out.println(i);
+        }
+        System.out.println("Happy new year");
+
+        // When i'm trick breakpoint it's coming (args = {String[0]@773})
+    }
 }
